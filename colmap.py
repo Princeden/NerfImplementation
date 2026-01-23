5b4777e0bc97855a5b3366840369353a52a07386
Princeden <princedenhom@gmail.com>
2026-01-23 14:41:36 +0000

    use gpu instead of cpu

--- colmap.py
+++ colmap.py
@@ -9,17 +9,20 @@ class COLMAP:
     output_dir: directory to create path
     """
 
-    def __init__(self, image_path, output_path):
+    def __init__(self, image_path, output_path, device):
         self.image_path = image_path
         self.output_path = Path(output_path)
         self.db_path = Path(image_path).joinpath("database.db")
         self.db_path.touch()
         self.camera_intrinscs = {}
+        self.device = device
 
     def extract_intrinsics(self):
-        pycolmap.extract_features(self.db_path, self.image_path)
-        pycolmap.match_exhaustive(self.db_path)
+        pycolmap.extract_features(self.db_path, self.image_path, self.device)
+        pycolmap.match_exhaustive(self.db_path, self.device)
         num_images = pycolmap.Database(self.db_path).num_images
+        print(num_images)
+        
         # reconstruction = pycolmap.incremental_mapping(
         #     database_path=self.db_path,
         #     image_path=self.image_path,
@@ -33,5 +36,5 @@ class COLMAP:
 
 
 if __name__ == "__main__":
-    colmap = COLMAP("./colmapData/cup", "./data/")
+    colmap = COLMAP("./colmapData/cup", "./data/", pycolmap.Device.cuda)
     colmap.extract_intrinsics()
