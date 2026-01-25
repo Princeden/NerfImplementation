8be3699b2e3b5bb44efbc806d3f093e95a03908e
Princeden Hom <princedenhom@gmail.com>
2026-01-25 17:11:04 -0500

    add colmap data extraction for nerf

--- colmap.py
+++ colmap.py
@@ -1,5 +1,8 @@
 import pycolmap
 from pathlib import Path
+import imageio
+from graphics_utils import pose_spherical
+import numpy as np
 
 
 class COLMAP:
@@ -17,41 +20,53 @@ class COLMAP:
         self.camera_intrinscs = {}
         self.device = device
 
-    def extract_intrinsics(self):
+    def get_reconstruction(self):
         pycolmap.extract_features(str(self.db_path), str(self.image_path))
         pycolmap.match_exhaustive(str(self.db_path))
         reconstruction = pycolmap.incremental_mapping(
-                str(self.db_path),
-                str(self.image_path),
-                str(self.image_path)
+            str(self.db_path), str(self.image_path), str(self.image_path)
         )[0]
-        print(reconstruction)
-        db = pycolmap.Database.open(self.db_path)
+        return reconstruction
+
+    """
+        Returns:
+        images: All images [N, H, W, 3 or 4]
+        poses: All camera poses [N, 4, 4]
+        render_poses: Novel view poses [N_render, 4, 4]
+        hwf: [H, W, focal]
+    """
+
+    def get_nerf_data(self):
+        reconstruction = self.get_reconstruction()
         focal_length = 0
         for camera_id in reconstruction.cameras:
             focal_length = reconstruction.camera(camera_id).focal_length_x
-        #pycolmap.incremental_mapping(str(self.db_path), str(self.image_path), str(self.image_path))
+        all_images = []
+        poses = []
+        render_poses = []
         for image in reconstruction.images.values():
+            im_data = imageio.imread(image.name)
+            all_images.append(im_data)
             if not image.has_frame_ptr():
-                print("img not regist")
+                print("img not registered")
                 continue
             w2c = image.cam_from_world()
             c2w = w2c.inverse()
-            print(c2w)
-            print(c2w.todict())
-
-        # reconstruction = pycolmap.incremental_mapping(
-        #     database_path=self.db_path,
-        #     image_path=self.image_path,
-        #     output_path=self.output_path,
-        # )
-        # print(reconstruction)
-        return
-
-    def get_nerf_json(self):
-        return
+            poses.append[c2w]
+        H, W = all_images[0].shape[:2]
+        render_poses = np.stack(
+            [
+                pose_spherical(angle, -30.0, 4.0)
+                for angle in np.linspace(-180, 180, 40 + 1)[:-1]
+            ],
+            dim=0,
+        )
+        return all_images, poses, render_poses, [H, W, focal_length]
 
 
 if __name__ == "__main__":
     colmap = COLMAP("./testData", "./data/", pycolmap.Device.cuda)
-    colmap.extract_intrinsics()
+    all_images, poses, render_poses, hwf = colmap.get_nerf_data()
+    print(all_images)
+    print(poses)
+    print(render_poses)
