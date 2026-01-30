b748b08cda75864b6aeb39b3fd52ca68e6bead20
Princeden Hom <princedenhom@gmail.com>
2026-01-30 15:38:14 -0500

    more cli options

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import argparse
 from colmap import COLMAP
 from model import train
 from pathlib import Path
+from graphics_utils import load_blender_data
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="NerfColmap Pipeline",
         description="Command Line interface to train and implement NeRFs (neural radiance fields) from any set of images utilizing the Colmap image reconstruction library",
+    )
+    parser.add_argument(
+        "--use_colmap", action=argparse.BooleanOptionalAction, default=False
     )
     parser.add_argument(
         "--input", "-i", type=Path, required=True, help="Input dataset directory"
@@ -27,7 +31,19 @@ if __name__ == "__main__":
         "--save_images", action=argparse.BooleanOptionalAction, default=False
     )
     args = parser.parse_args()
-    colmap = COLMAP(args.input, args.output)
-    print("Reconstructing Images for Camera Intrinsics")
-    all_images, poses, render_poses, hwf = colmap.get_nerf_data()
-    train(args.input, args.output, all_images, render_poses, hwf)
+    if args.use_colmap:
+        colmap = COLMAP(args.input, args.output)
+        print("Reconstructing Images for Camera Intrinsics")
+        all_images, poses, render_poses, hwf = colmap.get_nerf_data()
+    else:
+        print("Loading data from JSON")
+        all_images, poses, render_poses, hwf, i_split = load_blender_data(args.input)
+    train(
+        args.input,
+        args.output,
+        all_images,
+        render_poses,
+        hwf,
+        args.save_checkpoints,
+        args.save_images,
+    )
